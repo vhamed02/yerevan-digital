fdd35602029decd77b4ab9692782ea032bcf803b
Vendora Dev <vhamed02@gmail.com>
2026-05-17 07:32:47 +0000

    Randomize deploy banner background color on each deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -66,5 +66,7 @@ $COMPOSE exec -T nginx nginx -s reload
 echo "Nginx reloaded."
 
 printf '\033[1;30;102m Deploy finished: %s \033[0m\n' "$(date -Iseconds)"
-printf '\033[1;97;44m %s \033[0m\n' "$(git log -1 --pretty='%h %s')"
+_BG_COLORS=(41 42 43 44 45 46 101 102 104 105 106)
+_RAND_BG=${_BG_COLORS[$RANDOM % ${#_BG_COLORS[@]}]}
+printf "\033[1;97;${_RAND_BG}m %s \033[0m\n" "$(git log -1 --pretty='%h %s')"
 echo "========================================"
